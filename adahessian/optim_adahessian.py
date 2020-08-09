1109010aa49b6bdd4ae953ffad34f092405f38ec
Less Wright <lessw@etrillium.com>
2020-08-09 10:30:23 -0700

    Update optim_adahessian.py

--- adahessian/optim_adahessian.py
+++ adahessian/optim_adahessian.py
@@ -24,6 +24,20 @@ from torch.optim.optimizer import Optimizer
 from copy import deepcopy
 import numpy as np
 
+# imported from utils to avoid needing two imports... @lessw2020
+def get_params_grad(model):
+    """
+    get model parameters and corresponding gradients
+    """
+    params = []
+    grads = []
+    for param in model.parameters():
+        if not param.requires_grad:
+            continue
+        params.append(param)
+        grads.append(0. if param.grad is None else param.grad + 0.)
+    return params, grads
+
 
 class Adahessian(Optimizer):
     """Implements Adahessian algorithm.
