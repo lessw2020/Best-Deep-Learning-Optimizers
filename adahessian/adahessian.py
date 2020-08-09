09ac93fbaad575e9e85dea6e75fd8be4ecb47b10
Less Wright <lessw@etrillium.com>
2020-08-09 10:32:47 -0700

    Update adahessian.py

--- adahessian/adahessian.py
+++ adahessian/adahessian.py
@@ -3,6 +3,7 @@
 # Copyright (c) Zhewei Yao, Amir Gholami, Sheng Shen
 # All rights reserved.
 # This file is part of AdaHessian library.
+# source: https://github.com/amirgholami/adahessian
 #
 # AdaHessian is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
